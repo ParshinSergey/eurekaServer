39a4190547912417d7f19ba2d365b26c395f7b88
ParshinSergey <t2transformer@yahoo.com>
2024-10-11 23:16:53 +0300

    add Client2 and alias

--- client-2/src/main/java/org/example/controllers/HumanController.java
+++ client-2/src/main/java/org/example/controllers/HumanController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AnimalController {
+public class HumanController {
 
-    @RequestMapping("/animal")
-    public String getAnimal(){
-        return "Elefant and Monkey";
+    @RequestMapping("/human")
+    public String getHuman(){
+        return "Men and Woman";
     }
 }
