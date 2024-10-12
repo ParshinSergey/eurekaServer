ad1671cae565392450e7d6539d66acb26ade5ae8
ParshinSergey <t2transformer@yahoo.com>
2024-10-13 00:59:32 +0300

    multi instance in Client

--- eureka-client/src/main/java/org/example/controllers/AnimalController.java
+++ eureka-client/src/main/java/org/example/controllers/AnimalController.java
@@ -1,13 +1,17 @@
 package org.example.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AnimalController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
     @RequestMapping("/animal")
     public String getAnimal(){
-        return "Elefant and Monkey";
+        return "Elefant and Monkey. Instance-id = " + id;
     }
 }
